1cf7c14c7a70891fe0b62f9f5f3d8bd95e0aede1
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2022-12-22 20:56:58 +0900

    Create collatz_conjecture_plot.py

---
+++ collatz_conjecture_plot.py
@@ -0,0 +1,46 @@
+from matplotlib import pyplot as plt
+import tkinter as tk      
+from functools import partial
+
+def call_result(label_result, arg):
+    try:
+        number = int(arg.get())
+        startNum = number
+        yaxis = []
+        
+        while number != 1:
+            yaxis.append(number)
+        
+            if number % 2 == 0:
+                number /= 2
+            else:
+                number = number * 3 + 1
+        
+        yaxis.append(number)
+        print("finish!")            
+        print(yaxis)
+        
+        plt.plot(yaxis)
+        plt.title("Collatz conjecture graph starts with " + str(startNum))  
+        plt.show()
+        return
+
+    except ValueError:
+        label_result.config(text="Please enter number") 
+
+root = tk.Tk()                                                                  
+root.geometry('480x270+100+100')  
+root.title('Collatz conjecture plot')  
+   
+inputStr = tk.StringVar()  
+  
+labelNum = tk.Label(root, text="Enter number").grid(row=1, column=0)  
+labelResult = tk.Label(root)
+labelResult.grid(row=7, column=2)  
+
+entry = tk.Entry(root, textvariable=inputStr).grid(row=1, column=1)  
+  
+call_result = partial(call_result, labelResult, inputStr)  
+buttonPlot = tk.Button(root, text="plot", command=call_result).grid(row=3, column=1)  
+  
+root.mainloop()  
